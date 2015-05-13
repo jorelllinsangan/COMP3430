9b29dccdfa4a2a140d9ffca5bfdf7809febe4a00
maxewaschuk <ewaschukmps@gmail.com>
2015-05-13 16:31:41 -0500

    changes

--- fsc.c
+++ fsc.c
@@ -2,17 +2,46 @@
 #include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <assert.h>
+
+
+void tokenize(char* input, char** tolkien);
 
 int main(void){
  
  char* prompt = "COMP3430 Shell $ ";
  char* input;
- char* tolkien;
+ char** tolkien; // = malloc(sizeof(char*) * 100);
+ int i = 0;
 
  input = readline(prompt);
- tolkien = strtok(input, " \t");
+ tokenize(input, tolkien);
 
- printf("%s\n", input);
+ // while(tolkien[i]){
+	//  printf("%s\n", tolkien[i]);
+	//  i++;
+ // }
+
+}
+
+
+void tokenize(char* input, char** tokens){
+	assert(NULL != input);
+
+	char* token = strtok(input, " \t");
+	// int index = 0;
+
+	// // while(token != NULL){
+	// // 	tokens[index] = token;
+	// // 	token = strtok(NULL, " \t");
+	// // 	index++;
+	// // }
+
+	while (token != NULL)
+	{
+		printf("%s\n", token);
+		token = strtok(NULL, " \t");
+	}
 
 
 }
