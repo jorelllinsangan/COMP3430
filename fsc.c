f372de68b2193fe8c56543d2249f9e47abf04de8
maxewaschuk <ewaschukmps@gmail.com>
2015-05-15 18:10:58 -0500

    - Added a 'flushBuffer' function which ensures the input buffer is always initialized to NULL before reading in new user input (so we don't have left over values from previous instructions) - Implemented 'exit' command functionality. 'exit' closes the shell and returns the last commands value, 'exit <var>' exits and returns 'var'. - Changed 'cd' command to accept directories with spaces in their names. - Added 'isNumber' function to check if an argument is an integer value (used to check for valid arguments of 'exit' command)

--- fsc.c
+++ fsc.c
@@ -7,57 +7,103 @@
 #include <assert.h>
 #include <errno.h>
 
-
 void tokenize(char* input, char** tolkien);
-void start_shell();
-void execute(char** strArray);
+void flushBuffer(char** strArray);
+int start_shell();
+int execute(char** strArray, int* loop);
+int isNumber(char* word);
 
 int main(void){
-    start_shell();
+	int exitVal = start_shell();
+	return exitVal;
 }
 
-void start_shell()
+int start_shell()
 {
     char* prompt = "COMP3430 Shell $ ";
     char* input;
     char* strArray[40];
     //char* exitCommand;
-    int i = 0;
+    //int i = 0;
+    // Value returned by last executed function.
+    int lastVal = 0;
+    int loop = 1;
     
-    while(1)
+    while(loop)
     {
     	input = readline(prompt);
     	tokenize(input, strArray);
-    	execute(strArray);	
+
+		lastVal = execute(strArray, &loop);	
     }
-    
-    
-    while(strArray[i]){
-     printf("%s\n", strArray[i]);
-     i++;
-    }
+      
+    // while(strArray[i]){
+    //  printf("%s\n", strArray[i]);
+    //  i++;
+    // }
+
+    return lastVal;
 }
 
 // executes the command using the given arguments 
 // we expect that index 0 will be the command 
 // and any succeeding values will be arguments
-void execute(char** strArray)
+int execute(char** strArray, int* loop)
 {
 	char* command = strArray[0];
-	int exitValue = 1;
+	char* directoryStr;
+	int exitValue = 0;
 	char cwd[1024];
+	int index;
+
 	if(strcmp(command, "cd") == 0)
 	{
-		if(chdir(strArray[1]) == -1)
+		
+		if(NULL == strArray[1])
 		{
-			perror("cd");
+			// If no parameter given, return to home directory.
+			exitValue = chdir(getenv("HOME"));
 		}
+		else
+		{
+			// Handles directories with filenames that contain spaces.
+			index = 2;
+			directoryStr = strArray[1];
+			while(NULL != strArray[index]){
+				directoryStr = strcat(directoryStr, " ");
+				directoryStr = strcat(directoryStr, strArray[index]);
+				index++;
+			}
+
+			exitValue = chdir(directoryStr);
+
+			if(exitValue == -1){
+				printf("%s: No such file or directory.\n", directoryStr);
+			}
+		}
+
 
 	}
 	else if(strcmp(command, "exit") == 0)
 	{
-		//im not sure what I should be doing if exit has an argument.
-		exit(exitValue);
+		//Check if valid exit values have been passed.
+		if(NULL != strArray[1]){
+			if(NULL != strArray[2]){
+				printf("Invalid number of arguments.\n");
+
+			}else{
+				if(isNumber(strArray[1])){
+					exitValue = strtol(strArray[1],(char**)NULL,10);
+					*loop = 0;
+
+				}else{
+					printf("%s: Invalid parameter.\n",strArray[1]);
+
+				}
+			}
+		}else{
+			*loop = 0;
+		}
 	}
 	else if(strcmp(command, "pwd") == 0)
 	{
@@ -68,15 +114,20 @@ void execute(char** strArray)
 	}
 	else
 	{
-		printf("%s: command not found\n", command);
+		printf("%s: Command not found.\n", command);
 	}
 
+	return exitValue;
 }
+
+// Converts user input into an array of string tokens to be parsed.
 void tokenize(char* input, char** tokens){
 	assert(NULL != input);
 
 	char* token = strtok(input, " \t");
 	int index = 0;
+
+	flushBuffer(tokens);
 
 	while (token != NULL)
 	{	
@@ -86,4 +137,26 @@ void tokenize(char* input, char** tokens){
 		token = strtok(NULL, " \t");
 		index++;
 	}
+}
+
+/* Initializes all values in the buffer to NULL before tokenizing
+ * a command string.
+ */
+void flushBuffer(char** strArray){
+	for(int i = 0; i < 40; i++){
+		strArray[i] = NULL;
+	}
+}
+
+// Checks if a string is a number
+int isNumber(char* word){
+	int result = 1;
+
+ 	for(int i = 0; i < strlen(word) && result; i++){
+  		if(!isdigit(word[i])){
+ 			result = 0;
+  		}
+  	}
+
+	return result;
 }
